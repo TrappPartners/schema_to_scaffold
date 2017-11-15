48a3617cad654937b3809f9238a9b9d064ca00d8
jesusmar <jesusmarlima@gmail.com>
2017-11-14 20:18:46 -0500

    Only allow user_id to be generated as a reference

--- lib/schema_to_scaffold/attribute.rb
+++ lib/schema_to_scaffold/attribute.rb
@@ -14,8 +14,14 @@ module SchemaToScaffold
     def self.parse(attribute)
       match = attribute.match(/t\.(\w+)\s+"(\w+)"/)
       if match
-        name = match.captures[1].sub(/_id$/, "")
-        type = $&.nil? ? match.captures[0] : "references"
+        is_user = match.captures[1].sub(/_id$/, "") == "user"
+        if is_user
+          name = match.captures[1].sub(/_id$/, "")
+          type = "references"
+        else
+          name = match.captures[1]
+          type = match.captures[0]
+        end
         Attribute.new(name, type)
       end
     end
